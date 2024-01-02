c5e7e21dac59d57c52dba8971b8b5ed977e48d58
Kyle Greene <kmgreene@gmail.com>
2024-01-02 08:08:04 -0800

    Update image and fix site title.

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Kyle';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Meal Planner App';
 
 export default function Layout({ children, home }) {
     return (
